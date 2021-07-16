924b4f3e002ba4bd89bf34207b18e64967d8cb81
minjuice1 <minjuice93@gmail.com>
2021-07-16 20:44:58 +0900

    Signup.js 프레임 구성 & app.js 라우팅

---
+++ src/pages/Signup.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+const Signup = () => {
+
+    return (
+        <React.Fragment>
+            <div>
+                <div>인스타그램</div>
+                <div>친구들의 사진과 동영상을 보려면 가입하세요</div>
+                <div>face book으로 로그인</div>
+                <div>- 또는 -</div>
+            <div>
+                <div>이메일 주소</div>
+                <div>성명</div>
+                <div>사용자 이름</div>
+                <div>비밀번호</div>
+                <div>가입</div>
+            </div>
+            <div>계정이 있으신가요? 로그인</div>
+            <div>
+                <div>앱을 다운로드하세요.</div>
+                <div>앱스토어 / 구글플레이</div>
+            </div>
+            <div>소개 블로그 채용정보 도움말 API 개인정보</div>
+            <div>한국어 / 2021 instargram</div>
+            </div>
+            
+        </React.Fragment>
+    )
+}
+
+export default Signup;
--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,13 @@
-import './App.css';
 import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import Signup from '../pages/Signup';
 
 function App() {
   return (
     <React.Fragment>
-
+      <BrowserRouter>
+      <Route path="/signup" exact component={Signup}/>
+      </BrowserRouter>
     </React.Fragment> 
   );
 }
