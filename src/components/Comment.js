2a5adcccea76f036285e5e7dc624ed6d046fc893
minjuice1 <minjuice93@gmail.com>
2021-07-21 20:41:43 +0900

    Comment page 디자인 업데이트 중

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -11,23 +11,27 @@ import {actionCreators as postActions} from "../redux/modules/post"
 
 const Comment = (props) => {
 
+  const dispatch = useDispatch();
+  const is_me = useSelector((state) => state.comment.commentId);
+
+
   return (
-      <Grid is_flex width="auto">
+      <Grid is_flex width="auto" padding="0px 25px">
         
-      <Grid is_flex width="auto">
+      <Grid is_flex width="auto" margin="0px 20px 0px 0px">
               <Image shape="circle" src={props.image_url}/>
               <Text bold>{props.writer}</Text>
           </Grid>
           <Grid is_flex margin="0px 4px" width="auto">
-              <Text margin="0px">{props.content}</Text>
-              <FcLikePlaceholder size="22px"/>
-              {/* <MdDelete 
-              onClick={(e)=>{dispatch(commentActions.deleteCommentAPI(props.commentId))}}
-              /> */}
+              <Text margin="0px 100px 0px 0px">{props.content}</Text>
+              <FcLikePlaceholder size="22px" />
+              {is_me? <MdDelete onClick={(e)=>{dispatch(commentActions.deleteCommentAPI(props.commentId))}}/> : "" }
+                       
           </Grid>
       </Grid>
   )
 }
+
 
 
 
